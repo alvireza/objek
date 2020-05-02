43f0bc048a212499873003d6e0d6ccccebf9bbbb
alvireza <62271791+alvireza@users.noreply.github.com>
2020-05-02 16:01:13 +0800

    Initial commit

---
+++ src/dog.java
@@ -0,0 +1,28 @@
+public class dog {
+        String nama;
+        String jenis;
+        String warna;
+        int umur;
+        int berat;
+
+        void perkenalan() {
+            System.out.println("nama   :" + nama);
+            System.out.println("jenis  :" + jenis);
+            System.out.println("warna  :" + warna);
+            System.out.println("umur   :" + umur  +"tahun");
+            System.out.println("berat  :" + berat +"kg");
+        }
+            void eat(){
+                System.out.println("anjing mencoba mencari makan ");
+            }
+
+            void chaseCat(){
+                System.out.println("anjing mengejar kucing dan menindihinya dg berat " + berat +"kg");
+            }
+
+            void counter(){
+                System.out.println("kucing mencakar dan bersembunyi");
+            }
+        }
+
+
---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+            dog anjing = new dog();
+            dog kucing = new dog();
+            anjing.nama="Mantan";
+            anjing.jenis="pitbull";
+            anjing.warna="coklat";
+            anjing.umur=9;
+            anjing.berat=10;
+
+            anjing.perkenalan();
+            anjing.eat();
+            anjing.chaseCat();
+            kucing.counter();
+
+    }
+}
