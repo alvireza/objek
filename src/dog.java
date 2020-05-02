1fdac6689556fe615ed0e7eeaad98322b3566bdf
alvireza <62271791+alvireza@users.noreply.github.com>
2020-05-02 16:05:46 +0800

    belajar objek

--- src/dog.java
+++ src/dog.java
@@ -1,28 +1,29 @@
 public class dog {
-        String nama;
-        String jenis;
-        String warna;
-        int umur;
-        int berat;
+    String nama;
+    String jenis;
+    String warna;
+    int umur;
+    int berat;
 
-        void perkenalan() {
-            System.out.println("nama   :" + nama);
-            System.out.println("jenis  :" + jenis);
-            System.out.println("warna  :" + warna);
-            System.out.println("umur   :" + umur  +"tahun");
-            System.out.println("berat  :" + berat +"kg");
-        }
-            void eat(){
-                System.out.println("anjing mencoba mencari makan ");
-            }
+    void perkenalan() {
+        System.out.println("nama   :" + nama);
+        System.out.println("jenis  :" + jenis);
+        System.out.println("warna  :" + warna);
+        System.out.println("umur   :" + umur + "tahun");
+        System.out.println("berat  :" + berat + "kg");
+    }
 
-            void chaseCat(){
-                System.out.println("anjing mengejar kucing dan menindihinya dg berat " + berat +"kg");
-            }
+    void eat() {
+        System.out.println("anjing mencoba mencari makan ");
+    }
 
-            void counter(){
-                System.out.println("kucing mencakar dan bersembunyi");
-            }
-        }
+    void chaseCat() {
+        System.out.println("anjing mengejar kucing dan menindihinya dg berat " + berat + "kg");
+    }
+
+    void counter() {
+        System.out.println("kucing mencakar dan bersembunyi");
+    }
+}
 
 
--- src/main.java
+++ src/main.java
@@ -12,6 +12,5 @@ public class main {
             anjing.eat();
             anjing.chaseCat();
             kucing.counter();
-
     }
 }
